67e7e0fac9bc0aeab716ffb84f2ce34d5df2f29d
cabanue <cabanue@iu.edu>
2022-03-16 19:12:57 -0400

    added credentials

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,22 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    ...otherPlugins,
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyDoVUvgsG9ZbpA-LnjuGMeVgr1JrcyUI2A",
+          authDomain: "capstone-sp22.firebaseapp.com",
+          projectId: "capstone-sp22",
+          storageBucket: "capstone-sp22.appspot.com",
+          messagingSenderId: "1050121475702",
+          appId: "1:1050121475702:web:bb16b98afc902ad0deb157",
+          measurementId: "G-GSJM35V8KH",
+        },
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
