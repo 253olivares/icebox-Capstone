c845df464ae85fd47b3108d3bf34259cea892834
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2022-03-27 22:55:18 -0400

    Create MobileNav.js

---
+++ src/components/MobileNav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "semantic-ui-css/semantic.css";
+import { Menu } from "semantic-ui-react";
+import { Link } from "gatsby";
+
+import "../css/styles.css";
+import burgerMenu from "../images/bars-solid.png";
+
+const UserNav = () => {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    );
+};
+
+export default UserNav;
