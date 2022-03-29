3e36759a06c585c8eb45f8510e2751a45d8ba87a
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2022-03-29 15:04:14 -0400

    Update MobileNav.js

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { Container, Segment, Menu } from 'semantic-ui-react';
+import React from "react";
+import { Link } from "gatsby";
+// import Navbar from '../../components/Navbar';
+import { Container, Segment, Menu } from "semantic-ui-react";
 
 
 
