03fdee98166cd0fa17392e22525be40957cc8743
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2022-03-29 14:59:30 -0400

    fixed

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Navbar from '../../components/Navbar';
 import { Container, Segment, Menu } from 'semantic-ui-react';
 
 import "../css/styles.css";
