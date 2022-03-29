c158b03f4c861e0f9b1cf534fcf81e64760aff02
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2022-03-29 16:27:48 -0400

    Update db.js
    stuffy

--- src/services/db.js
+++ src/services/db.js
@@ -1,5 +1,6 @@
 import app from "gatsby-plugin-firebase-v9.0";
 
+
 // initializes the auth, but we need to pass app to auth function
 import {
   getAuth,
@@ -11,12 +12,16 @@ import {
 // sign in function (?) - pulled from firebase docs
 const auth = getAuth(app);
 
+var GlobalVarIfLogged = false;
+
 export function signIn(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
       console.log("user signed in successfully");
+      console.log(GlobalVarIfLogged);
+      GlobalVarIfLogged = true;
       // ...
     })
     .catch((error) => {
@@ -31,6 +36,7 @@ export function signOutUser() {
     .then(() => {
       // Sign-out successful.
       console.log("user signed out");
+      GlobalVarIfLogged = false;
     })
     .catch((error) => {
       // An error happened.
