68457eb69247d7e854f206da6d28a3df29045899
cabanue <70549810+cabanue@users.noreply.github.com>
2022-04-13 16:35:14 -0400

    Update dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import { navigate } from "gatsby";
-import 'semantic-ui-css/semantic.css';
-import { Link } from 'gatsby';
-import { Container, Button } from 'semantic-ui-react';
+import "semantic-ui-css/semantic.css";
+import { Link } from "gatsby";
+import { Container, Button } from "semantic-ui-react";
 import "../css/styles.css";
-
 
 import MobileNav from "../components/MobileNav";
 import UserNav from "../components/NavLogged";
@@ -17,77 +16,125 @@ import FridgeImage from "../images/Icebox_assets/fridge_icon.svg";
 
 import State from "../state";
 
-
 import Footer from "../components/Footer";
 
 const DashboardPage = () => {
+  const state = React.useContext(State);
 
-    const state = React.useContext(State);
+  const [mobileNavOpen, setMobileNavOpen] = React.useState(false);
+  //   const [houses, setHouses] = React.useState();
+  const openCloseNav = () => {
+    setMobileNavOpen(!mobileNavOpen);
+    {
+      mobileNavOpen
+        ? (document.body.style.overflow = "scroll")
+        : (document.body.style.overflow = "hidden");
+    }
+  };
 
-    const [mobileNavOpen, setMobileNavOpen] = React.useState(false);
-    const openCloseNav = () => {
-        setMobileNavOpen(!mobileNavOpen);
-        { mobileNavOpen ? document.body.style.overflow = "scroll" : document.body.style.overflow = "hidden"; }
-    };
+  const initialHouses = [];
 
+  const [listHouses, setListHouses] = React.useState(initialHouses);
 
-    return (
-        <React.Fragment>
-            <main>
-                {state.user ? <><UserNav openCloseNav={openCloseNav}></UserNav>{mobileNavOpen ? <MobileNavLogged ></MobileNavLogged> : null}</> : <><Nav openCloseNav={openCloseNav}></Nav>{mobileNavOpen ? <MobileNav ></MobileNav> : null}</>}
-                <Container className='dashboardName'>
-                    <div className='dashcontainer'>
-                        <img src={DashImage} className='dashboardImage'></img>
-                        <div className='dashboardInformation'>
-                            <h1><span className='userName'>
-                                {state.user ? <>{state.user.displayName}</> : <>User's Name</>}
-                            </span> DASHBOARD</h1>
-                            <p>Number of Households: X</p>
-                            <p>Number of Fridges: X</p>
-                        </div>
-                    </div>
-                </Container>
+  React.useEffect(() => {
+    if (state.fridges) {
+      const houses = state.fridges;
+      console.log(houses);
+      const houseList = houses.map((house, index) => {
+        return (
+          <React.Fragment key={`house-${index}`}>
+            <div className="householdFridge_One">
+              <img src={HouseImage} width="125px" height="100px"></img>
+              <p>
+                <span>{house.houseName}</span>
+              </p>
+              <p>1 Fridge</p>
+            </div>
+          </React.Fragment>
+        );
+      });
+      setListHouses(houseList);
+      console.log(listHouses);
+    } else {
+    }
+  }, [""]);
 
-                <Container className='dashboardHouseholds'>
-                    <div className='houseContainer'>
-                        <h1 className='hCHead'>HOUSEHOLDS <Button className='addH'> <Link to="/addh"> Add House</Link></Button></h1>
-                        <div className='householdsContainer'>
-                            <div className='householdFridge_One'>
-                                <img src={HouseImage} width="125px" height="100px"></img>
-                                <p><span>Household</span> Name</p>
-                                <p>1 Fridge</p>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
+  return (
+    <React.Fragment>
+      <main>
+        {state.user ? (
+          <>
+            <UserNav openCloseNav={openCloseNav}></UserNav>
+            {mobileNavOpen ? <MobileNavLogged></MobileNavLogged> : null}
+          </>
+        ) : (
+          <>
+            <Nav openCloseNav={openCloseNav}></Nav>
+            {mobileNavOpen ? <MobileNav></MobileNav> : null}
+          </>
+        )}
+        <Container className="dashboardName">
+          <div className="dashcontainer">
+            <img src={DashImage} className="dashboardImage"></img>
+            <div className="dashboardInformation">
+              <h1>
+                <span className="userName">
+                  {state.user ? (
+                    <>{state.user.displayName}</>
+                  ) : (
+                    <>User's Name</>
+                  )}
+                </span>{" "}
+                DASHBOARD
+              </h1>
+              <p>Number of Households: X</p>
+              <p>Number of Fridges: X</p>
+            </div>
+          </div>
+        </Container>
 
-                <Container className='individualFrdges'>
-                    <div className='iFContainer'>
-                        <h1 className='iFCHead'>
-                            INDIVIDUAL FRIDGES <Button className='addF'> <Link to="/addf"> Add Fridge</Link></Button>
-                        </h1>
-                        <div className='fridges'>
-                            <Link to="/fridge">
-                                <div className='fridges_One'>
-                                    <img src={FridgeImage}></img>
-                                    <div className='drigeinformation'>
-                                        <h1>FRIDGE'S NAME</h1>
-                                        <hr></hr>
-                                        <p>Number of Items: X</p>
-                                        <p>Recent Expired Items: X</p>
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-                </Container>
+        <Container className="dashboardHouseholds">
+          <div className="houseContainer">
+            <h1 className="hCHead">
+              HOUSEHOLDS{" "}
+              <Button className="addH">
+                {" "}
+                <Link to="/addh"> Add House</Link>
+              </Button>
+            </h1>
+            <div className="householdsContainer">{listHouses}</div>
+          </div>
+        </Container>
 
-                <Footer></Footer>
-            </main>
-        </React.Fragment >
-    );
+        <Container className="individualFrdges">
+          <div className="iFContainer">
+            <h1 className="iFCHead">
+              INDIVIDUAL FRIDGES{" "}
+              <Button className="addF">
+                {" "}
+                <Link to="/addf"> Add Fridge</Link>
+              </Button>
+            </h1>
+            <div className="fridges">
+              <Link to="/fridge">
+                <div className="fridges_One">
+                  <img src={FridgeImage}></img>
+                  <div className="drigeinformation">
+                    <h1>FRIDGE'S NAME</h1>
+                    <hr></hr>
+                    <p>Number of Items: X</p>
+                    <p>Recent Expired Items: X</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </Container>
 
-
-}
+        <Footer></Footer>
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default DashboardPage;
